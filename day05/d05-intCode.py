1fedc93e3535deec515b35ad3cd48bafe1bc57e1
Hai-Nam Nguyen <hainam@jcisio.com>
2019-12-09 11:45:44 +0100

    Day 9: refactoring intCode to prepare for new instructions and mode

--- day05/d05-intCode.py
+++ day05/d05-intCode.py
@@ -1,4 +1,13 @@
-def runIntCode(values, input):
+values = []
+
+def getParam(mode, param):
+  if (mode == 0):
+    return values[param]
+  else:
+    return param
+
+def runIntCode(input):
+  global values
   output = []
   i = 0
 
@@ -25,8 +34,8 @@ def runIntCode(values, input):
     if instruction == 99:
       break
     elif instruction == 1 or instruction == 2:
-      a = values[values[i+1]] if modes[0] == 0 else values[i+1]
-      b = values[values[i+2]] if modes[1] == 0 else values[i+2]
+      a = getParam(modes[0], values[i+1])
+      b = getParam(modes[1], values[i+2])
       values[values[i+3]] = a+b if instruction == 1 else a*b
       i += 4
     elif instruction == 3:
@@ -35,19 +44,19 @@ def runIntCode(values, input):
       values[values[i+1]] = input.pop()
       i += 2
     elif instruction == 4:
-      a = values[values[i+1]] if modes[0] == 0 else values[i+1]
+      a = getParam(modes[0], values[i+1])
       output.append(a)
       i += 2
     elif instruction == 5 or instruction == 6:
-      a = values[values[i+1]] if modes[0] == 0 else values[i+1]
-      b = values[values[i+2]] if modes[1] == 0 else values[i+2]
+      a = getParam(modes[0], values[i+1])
+      b = getParam(modes[1], values[i+2])
       if (a == 0) == (instruction == 6):
         i = b
       else:
         i += 3
     elif instruction == 7 or instruction == 8:
-      a = values[values[i+1]] if modes[0] == 0 else values[i+1]
-      b = values[values[i+2]] if modes[1] == 0 else values[i+2]
+      a = getParam(modes[0], values[i+1])
+      b = getParam(modes[1], values[i+2])
       values[values[i+3]] = 1 if (instruction == 7 and a < b) or (instruction == 8 and a == b) else 0
       i += 4
 
@@ -57,5 +66,5 @@ def runIntCode(values, input):
   return ' '.join(map(str, output))
 
 import fileinput
-data = [line.strip().split(',') for line in fileinput.input()]
-print(runIntCode(data[0], data[1]))
+values, input = [line.strip().split(',') for line in fileinput.input()]
+print(runIntCode(input))
