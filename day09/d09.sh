fb761942d6c17146634a1ca58972bba98e55b2ec
Hai-Nam Nguyen <hainam@jcisio.com>
2019-12-09 17:13:35 +0100

    Display part 1/2 as in previous days

--- day09/d09.sh
+++ day09/d09.sh
@@ -1,2 +1,5 @@
+#!/usr/bin/env bash
+echo -n "Part 1: "
 echo -e "$(cat d09.in)\n1" | python3 ../day05/d05-intCode.py
+echo -n "Part 2: "
 echo -e "$(cat d09.in)\n2" | python3 ../day05/d05-intCode.py
