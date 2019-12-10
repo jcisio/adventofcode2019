8e4b266c6055f5ec2513a3d6a0e24f99bdb2a5fb
Hai-Nam Nguyen <hainam@jcisio.com>
2019-12-10 10:06:56 +0100

    Day 10 part 2

--- day10/d10.py
+++ day10/d10.py
@@ -16,7 +16,7 @@ def hasObstacles(o1, o2):
 objects = []
 M, N = len(amap[0]), len(amap)
 for i in range(M*N):
-    x, y = i%M, i//N
+    x, y = i%M, i//M
     if amap[y][x] == '#':
         objects.append((x,y))
 
@@ -32,3 +32,44 @@ for i in range(len(objects) - 1):
             degrees[j] += 1
 
 print('Part 1:', max(degrees))
+
+station = objects[degrees.index(max(degrees))]
+shots = {}
+import math
+for o in objects:
+    if o == station:
+        continue
+    dx = o[0] - station[0]
+    dy = o[1] - station[1]
+    angle = math.atan2(-dx, dy)*180/math.pi
+    # Up is 0 degree, convert to that system.
+    angle += 180
+    if angle >= 360:
+        angle -= 360
+    if not angle in shots:
+        shots[angle] = {}
+    shots[angle][o] = dx*dx + dy*dy
+
+n_current = 0
+n_stop = 200
+while True:
+    for i in sorted(shots.keys()):
+        n_current += 1
+        dmin = M*M+N*N
+        for k in shots[i]:
+            if shots[i][k] < dmin:
+                dmin = shots[i][k]
+                current_object = k
+        del shots[i][current_object]
+        if not shots[i]:
+            del shots[i]
+        if n_current == n_stop:
+            break
+    else:
+        if not shots:
+            break
+        continue
+    break
+
+if n_current == n_stop:
+    print('Part 2:', current_object[0]*100+current_object[1])
