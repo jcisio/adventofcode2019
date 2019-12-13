c5166f1175afe7a76a467325e4abbf9b207ddf8d
Hai-Nam Nguyen <hainam@jcisio.com>
2019-12-13 09:44:00 +0100

    Day 13 visualization

--- day13/d13.py
+++ day13/d13.py
@@ -3,6 +3,12 @@ import sys
 sys.path.append('../common')
 import intcode
 
+TILE_EMPTY = 0
+TILE_WALL = 1
+TILE_BLOCK = 2
+TILE_PADDLE = 3
+TILE_BALL = 4
+
 values = list(map(int, open('d13.in').read().strip().split(',')))
 intcode.init(values)
 output = intcode.runIntCode([])
@@ -10,4 +16,22 @@ screen = {}
 for i in range(len(output)//3):
   x,y, tile = output[i*3:i*3+3]
   screen[(x,y)] = tile
-print('Part 1:', sum([tile==2 for tile in screen.values()]))
+
+def print_screen():
+  for y in range(20):
+    for x in range(40):
+      if screen[(x,y)] == TILE_EMPTY:
+        c = '  '
+      elif screen[(x,y)] == TILE_WALL:
+        c = '⬛️'
+      elif screen[(x,y)] == TILE_BLOCK:
+        c = '⬜️'
+      elif screen[(x,y)] == TILE_PADDLE:
+        c = '=='
+      elif screen[(x,y)] == TILE_BALL:
+        c = '⚫'
+      print(c, end='')
+    print()
+
+print_screen()
+print('Part 1:', sum([tile==TILE_BLOCK for tile in screen.values()]))
