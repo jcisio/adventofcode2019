41301e6afe8f465cbe57ebbc3c6faa3ae905917a
Hai-Nam Nguyen <hainam@jcisio.com>
2019-12-19 21:12:42 +0100

    Day 16 refactor part 2

--- day16/d16.py
+++ day16/d16.py
@@ -14,20 +14,24 @@ def get_output(input, iterations):
         input = [abs(sum([input[k]*base[(k+1)%(4*(j+1))//(j+1)] for k in range(L)])) % 10 for j in range(L)]
     return ''.join(map(str, input))
 
+
+def part2(input):
+    L = len(input)*N_REPEAT
+    offset = int(input[0:7])
+    if (offset < L/2):
+        sys.exit('Oh my, I have no idea deal with that offset.')
+
+    input = input*N_REPEAT
+    output = []
+    for i in range(offset, offset+8):
+        c = int(input[i])
+        coeff = 1
+        for j in range(i+1,L):
+            coeff = coeff*(N_ITERATION+j-i-1)//(j-i)
+            c += int(input[j])*coeff
+        output.append(c % 10)
+    return ''.join(map(str, output))
+
+
 #print('Part 1:', get_output(input, N_ITERATION)[0:8])
-
-L = len(input)*N_REPEAT
-offset = int(input[0:7])
-if (offset < L/2):
-    sys.exit('Oh my, I have no idea deal with that offset.')
-
-input = input*N_REPEAT
-output = []
-for i in range(offset, offset+8):
-    c = int(input[i])
-    coeff = 1
-    for j in range(i+1,L):
-        coeff = coeff*(N_ITERATION+j-i-1)//(j-i)
-        c += int(input[j])*coeff
-    output.append(c % 10)
-print('Part 2:', ''.join(map(str, output)))
+print('Part 2:', part2(input))
